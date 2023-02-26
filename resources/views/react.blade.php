bddc9685b06f705c9b9d97b398772a30f2d284f8
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-26 08:10:13 +0200

    react build test if will works

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -1,14 +1,19 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>33 In 1 App</title>
+    <meta charset="utf-8"/>
+    <link rel="icon" href="{{ asset('react-app/favicon.ico') }}"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+    <meta name="theme-color" content="#000000"/>
+    <meta name="description" content="Web site created using create-react-app"/>
+    <link rel="apple-touch-icon" href="{{ asset('react-app/logo192.png') }}"/>
+    <link rel="manifest" href="{{ asset('react-app/manifest.json') }}"/>
+    <title>33 in one</title>
+    <script defer="defer" src="{{ asset('react-app/static/js/main.03fee2c2.js') }}"></script>
+    <link href="{{ asset('react-app/static/css/main.073c9b0a.css') }}" rel="stylesheet">
 </head>
 <body>
+<noscript>You need to enable JavaScript to run this app.</noscript>
 <div id="root"></div>
-<script src="{{ asset('react-app/static/js/main.js') }}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{path?}', [ReactController::class, 'index'])
-    ->where('path', '.*');
+//Route::get('/{path?}', [ReactController::class, 'index'])
+//    ->where('path', '.*');
 
-//Route::any('/{any}', [ReactController::class,'index'])
-//    ->where('any',"^(?!api).*$");
+Route::any('/{any}', [ReactController::class,'index'])
+    ->where('any',"^(?!api).*$");
 
 //Route::get('/', function () {
 //    return view('welcome');
