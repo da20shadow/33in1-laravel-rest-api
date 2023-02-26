1ff2f1638878004787b64568f1cef356feb97406
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-26 07:20:24 +0200

    React setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{path?}', [ReactController::class, 'index'])
+    ->where('path', '.*');
+
+//Route::any('/{any}', [ReactController::class,'index'])
+//    ->where('any',"^(?!api).*$");
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
