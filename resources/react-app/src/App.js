e554357d71a4378852824768476efd5c2727e369
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-27 16:32:13 +0200

    added use effect

--- resources/react-app/src/App.js
+++ resources/react-app/src/App.js
@@ -6,21 +6,37 @@ import {
     Register,
     BodyComposition,
     AddBodyComposition,
-    EditBodyComposition
+    EditBodyComposition, Water, WaterStat, Sleep, Food, Work, Workout
 } from "./components";
 import {Routes,Route,Navigate} from "react-router-dom";
 import {useEffect, useState} from "react";
-import {authService} from "./services";
+import {authService, userService} from "./services";
+import {useStateContext} from "./context/ContextProvider";
 
 function App() {
-    const [isLogged,setIsLogged] = useState(false);
+    const {user,isLogged,loginUser,logoutUser} = useStateContext();
     useEffect(() => {
-        setIsLogged(authService.isLogged());
-        console.log('isLogged', isLogged)
+        console.log('App.js useEffect ContextProvider user',user)
+        let localStoreUser = localStorage.getItem('user');
+        console.log('App.js useEffect localStore user ', localStoreUser)
+        if (!isLogged || !localStoreUser) {
+            console.log('There is no info in the Context!')
+            console.log('Checking with request to the server...!')
+            userService.get().then(r => {
+                loginUser(r.token)
+                console.log(r)
+            }).catch(err => {
+                console.log('Error: ', err)
+                console.log('Error: ', err.message)
+                if (err.message === 'Unauthenticated.') {
+                    logoutUser()
+                }
+            });
+        }
     },[])
     return (
         <>
-            <Header isLogged={isLogged} />
+            <Header />
 
             <Routes>
                 <Route path={'/'} element={<Home/>} />
@@ -30,6 +46,12 @@ function App() {
                 <Route path={'/body-composition'} element={isLogged ?  <BodyComposition/> : <Navigate to="/login" replace={true} />} />
                 <Route path={'/body-composition/add'} element={isLogged ?  <AddBodyComposition/> : <Navigate to="/login" replace={true} />} />
                 <Route path={'/body-composition/edit'} element={isLogged ?  <EditBodyComposition/> : <Navigate to="/login" replace={true} />} />
+                <Route path={'/sleep'} element={isLogged ?  <Sleep/> : <Navigate to="/login" replace={true} />} />
+                <Route path={'/water/add'} element={isLogged ?  <Water/> : <Navigate to="/login" replace={true} />} />
+                <Route path={'/water/stat'} element={isLogged ?  <WaterStat/> : <Navigate to="/login" replace={true} />} />
+                <Route path={'/food'} element={isLogged ?  <Food/> : <Navigate to="/login" replace={true} />} />
+                <Route path={'/work'} element={isLogged ?  <Work/> : <Navigate to="/login" replace={true} />} />
+                <Route path={'/workout'} element={isLogged ?  <Workout/> : <Navigate to="/login" replace={true} />} />
             </Routes>
 
         </>
