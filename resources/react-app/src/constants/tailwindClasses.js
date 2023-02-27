b7866c789add7ec4c34242f5a3ecb397ec965c9f
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-27 16:34:52 +0200

    create button and input styles for tailwind

--- resources/react-app/src/constants/tailwindClasses.js
+++ resources/react-app/src/constants/tailwindClasses.js
@@ -1,10 +1,14 @@
 const navLink = 'px-2 py-1 flex-1 text-center text-lg font-bold bg-cyan-600 hover:bg-cyan-500 active:bg-cyan-400 rounded';
 const btnLg = 'px-4 py-2 text-xl font-bold bg-cyan-600 hover:bg-cyan-500 active:bg-cyan-400 rounded';
-const btnFullLg = 'w-full px-4 py-2 text-center text-xl font-bold bg-cyan-600 hover:bg-cyan-500 active:bg-cyan-400 rounded';
+const btnFullLg = 'w-full px-4 py-2 text-[#292d33] hover:text-[#454a52] text-center text-xl font-bold bg-cyan-600 ' +
+    'hover:bg-cyan-500 active:bg-cyan-400 border-2 border-transparent hover:border-gray-400 ' +
+    'hover:scale-105 active:scale-95 transition duration-200 ease-in rounded';
+const inputStyle = 'w-full p-2 mb-3 rounded bg-[#c6c6c6]';
 
 const tailwindClasses = {
     navLink,
     btnLg,
     btnFullLg,
+    inputStyle,
 }
 export default tailwindClasses
