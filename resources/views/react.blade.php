0972d50d2177a5e89e5df4b26b30a3af1eec9368
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-27 17:09:33 +0200

    build for prod

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,13 +5,12 @@
     <link rel="icon" href="{{ asset('react-app/favicon.ico') }}"/>
     <meta name="viewport" content="width=device-width,initial-scale=1"/>
     <meta name="theme-color" content="#000000"/>
-    <meta name="description" content="Web site created using create-react-app"/>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="33 in one app"/>
     <link rel="apple-touch-icon" href="{{ asset('react-app/logo192.png') }}"/>
     <link rel="manifest" href="{{ asset('react-app/manifest.json') }}"/>
     <title>33 in one</title>
-    <script defer="defer" src="{{ asset('react-app/static/js/main.03fee2c2.js') }}"></script>
-    <link href="{{ asset('react-app/static/css/main.073c9b0a.css') }}" rel="stylesheet">
+    <script defer="defer" src="{{ asset('react-app/static/js/main.98835ece.js') }}"></script>
+    <link href="{{ asset('react-app/static/css/main.6c3caaac.css') }}" rel="stylesheet">
 </head>
 <body>
 <noscript>You need to enable JavaScript to run this app.</noscript>
