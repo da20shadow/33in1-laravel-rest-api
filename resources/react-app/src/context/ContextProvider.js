c9da68a45edc7c225aff3debc8fdd307c51066db
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-27 16:34:59 +0200

    create context

---
+++ resources/react-app/src/context/ContextProvider.js
@@ -0,0 +1,30 @@
+import {createContext,useContext} from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const StateContext = createContext();
+
+const initialUserState = '';
+
+export const ContextProvider = ({children}) => {
+    const [user, setUser] = useLocalStorage('user', initialUserState);
+    const loginUser = (userData) =>{
+        setUser(userData);
+    }
+    const logoutUser = () =>{
+        setUser(initialUserState);
+    }
+
+    return (
+        <StateContext.Provider value={{
+            isLogged: user,
+            user,
+            setUser,
+            loginUser,
+            logoutUser,
+        }} >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
