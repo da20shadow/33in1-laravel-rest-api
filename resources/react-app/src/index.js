c73f25b0d917906cf7af97a0f586426bee235c58
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-27 16:35:18 +0200

    add the context

--- resources/react-app/src/index.js
+++ resources/react-app/src/index.js
@@ -5,13 +5,16 @@ import './assets/styles/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import {ContextProvider} from "./context/ContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <ContextProvider>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </ContextProvider>
     </React.StrictMode>
 );
 
