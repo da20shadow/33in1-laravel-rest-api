759a003aa9119a1946fca7995c08d9c8344bd160
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-28 21:37:35 +0200

    create footer

---
+++ resources/react-app/src/components/common/Footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return (
+        <footer className={'py-20 bg-gray-100'}>
+            <p className="text-center">2023 copyright &copy; da20shadow All right reserved.</p>
+        </footer>
+    )
+}
+export default Footer;
